fd4c64fdca1f088f0a1ae6ffbb03b20928a50e19
Divyansh Kamboj <divyansh.kamboj@accuknox.com>
2021-08-25 08:41:37 +0530

    localstorage: make the storageclass local-path as default
    Signed-off-by: Divyansh Kamboj <divyansh.kamboj@accuknox.com>

--- common.sh
+++ common.sh
@@ -79,6 +79,7 @@ handleLocalStorage(){
 	case $PLATFORM in
 		self-managed)
 			kubectl $1 -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+			kubectl annotate storageclass local-path storageclass.kubernetes.io/is-default-class="true"
 			;;
 		*)
 			echo "Skipping..."
