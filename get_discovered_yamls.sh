ef1c63f5bd0f2e6cfdb2232403207be36f76714d
Rahul Jadhav <nyrahul@gmail.com>
2022-01-19 00:37:40 +0530

    discovering yamls after trigger
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- get_discovered_yamls.sh
+++ get_discovered_yamls.sh
@@ -4,9 +4,16 @@ podname=$(kubectl get pod -n explorer -l container=knoxautopolicy -o=jsonpath='{
 [[ $? -ne 0 ]] && echo "could not find knoxautopolicy pod" && exit 2
 echo "Downloading discovered policies from pod=$podname"
 
+function trigger_policy_dump()
+{
+	kubectl exec -n explorer $podname -- ls /convert_$1_policy.sh 2>&1 >/dev/null
+	[[ $? -eq 0 ]] && kubectl exec -n explorer $podname -- /convert_$1_policy.sh
+}
+
 function network_policy()
 {
-	filelist=`kubectl exec -n explorer $podname -- ls -1 | grep "cilium_policies_.*\.yaml"`
+	trigger_policy_dump net
+	filelist=`kubectl exec -n explorer $podname -- ls -1 | grep "cilium_policies.*\.yaml"`
 	[[ "$filelist" == "" ]] && echo "No network policies discovered" && return
 	for f in `echo $filelist`; do
 		f=$(echo $f | tr -d '\r')
@@ -21,6 +28,7 @@ function network_policy()
 
 function system_policy()
 {
+	trigger_policy_dump sys
 	filelist=`kubectl exec -n explorer $podname -- ls -1 | grep "kubearmor_policies\.yaml"`
 	[[ "$filelist" == "" ]] && echo "No system policies discovered" && return 1
 	for f in `echo $filelist`; do
