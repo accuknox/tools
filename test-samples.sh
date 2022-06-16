181c7c1c29d82b72199f8c7193962db2666b5cad
Rahul Jadhav <nyrahul@gmail.com>
2022-06-17 00:47:15 +0530

    added a way to verify the png diff
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- test-samples.sh
+++ test-samples.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+TMPNG=/tmp/t.png
+
 for yaml in `ls -1 samples/*.yaml`; do
 	exp_png=${yaml/.yaml/.png}
 	echo "$yaml => $exp_png"
-	./filter-policy -f $yaml -g /tmp/t.png
-	diff /tmp/t.png $exp_png
-	if [ $? -ne 0 ]; then
-		echo "filter-policy graph output for $yaml is a mismatch"
+	rm $TMPNG
+	./filter-policy -f $yaml -g $TMPNG
+	[[ ! -f $TMPNG ]] && echo "Output File not created!!"
+	TSZ=`stat --printf="%s" $TMPNG`
+	tolerance=`echo $(($TSZ * 5/100))`
+	PNG=`stat --printf="%s" $exp_png`
+
+	X=`echo $(($TSZ - $tolerance))`
+	Y=`echo $(($TSZ + $tolerance))`
+
+	if [ $PNG -gt $X ] && [ $PNG -lt $Y ]; then
+		echo "PNGs are similar in size"
+	else
+		echo "too much variation in the generated png"
 		exit 1
 	fi
 done
