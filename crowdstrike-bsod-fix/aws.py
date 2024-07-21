94e905251b36abad6ab5611c55f5f9f8879702ff
Rahul Jadhav <nyrahul@gmail.com>
2024-07-22 00:21:35 +0530

    aws crowdstrike remediation
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

---
+++ crowdstrike-bsod-fix/aws.py
@@ -0,0 +1,139 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 AccuKnox
+# Copyright 2024 XCitium
+
+import logging
+import time
+import boto3
+import subprocess
+import os
+import glob
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("main")
+
+def detach_volumes(ec2, inst_id, dry_run=False):
+    try:
+        volumes = ec2.describe_volumes(
+            Filters=[{"Name": "attachment.instance-id", "Values": [inst_id]}]
+        )
+    except Exception as e:
+        if "DryRunOperation" not in str(e):
+            log.error(f"Failed to get volumes for instance {inst_id}: {e}")
+        return
+
+    for volume in volumes["Volumes"]:
+        vid = volume["VolumeId"]
+        try:
+            res = ec2.detach_volume(VolumeId=vid, DryRun=dry_run)
+            waiter = ec2.get_waiter("volume_available")
+            waiter.wait(VolumeIds=[vid])
+        except Exception as e:
+            if "DryRunOperation" not in str(e):
+                log.error(f"Failed to detach volume {vid}: {e}")
+            continue
+        yield vid, res["Device"]
+
+# Function to attach volume to the current instance
+def attach_volume(ec2, inst_id, vid, dev, dry_run=False):
+    try:
+        ec2.attach_volume(
+            InstanceId=inst_id, VolumeId=vid, Device=dev, DryRun=dry_run
+        )
+        waiter = ec2.get_waiter("volume_in_use")
+        waiter.wait(VolumeIds=[vid])
+    except Exception as e:
+        if "DryRunOperation" not in str(e):
+            log.error(f"Failed to attach volume {vid}: {e}")
+
+
+# Function to detach volume from the current instance
+def detach_volume(ec2, vid, dry_run=False):
+    try:
+        ec2.detach_volume(VolumeId=vid, DryRun=dry_run)
+        waiter = ec2.get_waiter("volume_available")
+        waiter.wait(VolumeIds=[vid])
+    except Exception as e:
+        if "DryRunOperation" not in str(e):
+            log.error(f"Failed to detach volume {vid}: {e}")
+
+# Function to get the device name
+def get_device_name():
+    res = subprocess.run(["fdisk -l"], capture_output=True, shell=True).stdout.decode()
+    for line in res.splitlines():
+        if "NTFS" in line:
+            device_name = line.split(" ")[0]
+            return device_name
+    return ""
+
+# Function to remove the file
+def remove_crowdstrike_file(mount_point):
+    file_pattern = f"{mount_point}/Windows/System32/drivers/CrowdStrike/C-00000291*.sys"
+    for file in glob.glob(file_pattern):
+        os.remove(file)
+
+# Function to start an instance
+def start_instance(ec2, inst_id, dry_run=False):
+    try:
+        ec2.start_instances(InstanceIds=[inst_id], DryRun=dry_run)
+        waiter = ec2.get_waiter("instance_running")
+        waiter.wait(InstanceIds=[inst_id])
+    except Exception as e:
+        if "DryRunOperation" not in str(e):
+            log.error(f"Failed to start instance {inst_id}: {e}")
+
+# Function to stop an instance
+def stop_instance(ec2, inst_id, dry_run=False):
+    try:
+        ec2.stop_instances(InstanceIds=[inst_id], DryRun=dry_run)
+        waiter = ec2.get_waiter("instance_stopped")
+        waiter.wait(InstanceIds=[inst_id])
+    except Exception as e:
+        if "DryRunOperation" not in str(e):
+            log.error(f"Failed to stop instance {inst_id}: {e}")
+
+def handle_aws(args):
+    log.info(f"called {args.csp}")
+    instances=args.instances.split(",")
+    mount_point = os.getcwd() + "/windows"
+    os.makedirs(mount_point, exist_ok=True)
+    log.info(f"mount directory {mount_point}")
+    ec2 = boto3.client('ec2')
+    # response = ec2.describe_instances()
+    # print(response)
+    # return
+
+    for inst_id in instances:
+        log.info(f"checking {inst_id}")
+        try:
+            stop_instance(ec2=ec2, inst_id=inst_id, dry_run=args.dry_run)
+        except Exception as err:
+            log.error(f"could not stop {inst_id}: {err}")
+            continue
+
+        for vid, dev in detach_volumes(ec2=ec2, inst_id=inst_id, dry_run=args.dry_run):
+            try:
+                attach_volume(ec2=ec2, inst_id=inst_id, vid=vid, dev="/dev/sdz", dry_run=args.dry_run)
+            except Exception as err:
+                if "DryRunOperation" not in str(err):
+                    log.error(f"attach vol failed {vid}: {err}")
+                    continue
+
+            try:
+                devname=""
+                for retry in range(20):
+                    devname=get_device_name()
+                    if devname:
+                        break
+                    time.sleep(1)
+                if not args.dry_run:
+                    subprocess.run(
+                        ["mount", devname, mount_point], capture_output=True
+                    )
+                    remove_crowdstrike_file(mount_point)
+                    subprocess.run(["umount", mount_point], capture_output=True)
+            finally:
+                detach_volume(ec2, vid, dry_run=args.dry_run)
+                attach_volume(ec2, inst_id, vid, dev, dry_run=args.dry_run)
+
+        start_instance(ec2, inst_id, dry_run=args.dry_run)
---
+++ crowdstrike-bsod-fix/azure.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 AccuKnox
+# Copyright 2024 XCitium
+
+def handle_azure(args):
+    print(f"called {args.csp}")
