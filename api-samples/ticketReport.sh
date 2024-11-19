40817f6379d6f8edaa80cd791ab906115eb4a51d
Rahul Jadhav <nyrahul@gmail.com>
2024-11-19 18:43:49 +0530

    simplified calling accuknox api
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- api-samples/ticketReport.sh
+++ api-samples/ticketReport.sh
@@ -2,34 +2,22 @@
 
 # To set .accuknox.cfg check https://github.com/accuknox/tools/tree/main/api-samples
 . ${ACCUKNOX_CFG:-~/.accuknox.cfg}
-
-# Other params
-TMP=/tmp/$$
+. util.sh
 
 get_ticket_details()
 {
-	curl $CURLOPTS "$CSPM_URL/api/v1/tickets/$1" \
-	  -H 'accept: */*' \
-	  -H "authorization: Bearer $TOKEN" \
-	  -H 'content-type: application/json' | jq .
+	ak_api "$CSPM_URL/api/v1/tickets/$1"
+	echo $json_string | jq .
 }
 
 get_ticket_list()
 {
+	ak_api "$CSPM_URL/api/v1/tickets?page=1&page_size=20&status=Opened&created_before=2024-11-13&created_after=2024-11-05"
 	while read line; do
 		echo $line
 		get_ticket_details $line
-	done < <(curl $CURLOPTS "$CSPM_URL/api/v1/tickets?page=1&page_size=20&status=Opened&created_before=2024-11-13&created_after=2024-11-05" \
-	  -H 'accept: */*' \
-	  -H "authorization: Bearer $TOKEN" \
-	  -H 'content-type: application/json' | jq -r '.results[].id')
+	done < <(echo $json_string | jq -r '.results[].id')
 }
-
-
-function cleanup {
-	rm -f $TMP 2>/dev/null
-}
-trap cleanup EXIT
 
 main()
 {
