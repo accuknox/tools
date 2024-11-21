43f7b42fc897d40abec4be56db30e58ac331118f
Rahul Jadhav <nyrahul@gmail.com>
2024-11-21 20:00:37 +0530

    bulk image scan
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

---
+++ imgscan/imagescan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SA_JSON="service_account.json"
+[[ "$SA_EMAIL" == "" ]] && echo "SA_EMAIL / ServiceAccount Email not provided" && exit 1
+[[ ! -f "$SA_JSON" ]] && echo "$SA_JSON file not found" && exit 1
+
+REGISTRY=${REGISTRY:-us-east1-docker.pkg.dev/kube-airgapped/accuknox-onprem}
+
+gcloud auth activate-service-account $SA_EMAIL --key-file=$SA_JSON
+
+for img in `gcloud artifacts docker images list "$REGISTRY" --include-tags --format=json | jq -r '.[] | "\(.package):\(.tags[])"' 2>/dev/null`; do
+	echo $img
+	rm -f report.json 2>/dev/null
+	trivy image $img --format json --timeout 3600s -o report.json > report.log 2>&1
+	[[ ! -f "report.json" ]] && echo "image scanning failed $img" && continue
+	curl -v -L -X POST "https://$AKURL/api/v1/artifact/?tenant_id=$TENANT&data_type=TR&label_id=$LABEL&save_to_s3=false" -H "Tenant-Id: $TENANT" -H "Authorization: Bearer $TOKEN" --form 'file=@"./report.json"'
+done
